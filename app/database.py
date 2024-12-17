0e3508801f54a15b1fcb603a01e8e725843eff8b
all4nitrous <all4nitrous@gmail.com>
2024-12-17 18:52:15 -0500

    changes to user_models and dockerfiles

--- app/database.py
+++ app/database.py
@@ -1,25 +1,39 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 
+# Define the Base object
 Base = declarative_base()
 
 class Database:
-    """Handles database connections and sessions."""
     _engine = None
     _session_factory = None
 
     @classmethod
     def initialize(cls, database_url: str, echo: bool = False):
-        """Initialize the async engine and sessionmaker."""
-        if cls._engine is None:  # Ensure engine is created once
-            cls._engine = create_async_engine(database_url, echo=echo, future=True)
-            cls._session_factory = sessionmaker(
-                bind=cls._engine, class_=AsyncSession, expire_on_commit=False, future=True
-            )
+        """
+        Initializes the async database engine and session factory.
+        """
+        cls._engine = create_async_engine(database_url, echo=echo, future=True)
+        cls._session_factory = sessionmaker(
+            bind=cls._engine,
+            class_=AsyncSession,
+            expire_on_commit=False
+        )
 
     @classmethod
-    def get_session_factory(cls):
-        """Returns the session factory, ensuring it's initialized."""
-        if cls._session_factory is None:
-            raise ValueError("Database not initialized. Call `initialize()` first.")
-        return cls._session_factory
+    def get_session(cls) -> AsyncSession:
+        """
+        Returns a new async session.
+        """
+        if not cls._session_factory:
+            raise RuntimeError("Database not initialized!")
+        return cls._session_factory()
+
+    @classmethod
+    async def close(cls):
+        """
+        Closes the async database engine.
+        """
+        if cls._engine:
+            await cls._engine.dispose()
--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -33,7 +33,9 @@ from app.services.jwt_service import create_access_token
 from app.utils.link_generation import create_user_links, generate_pagination_links
 from app.dependencies import get_settings
 from app.services.email_service import EmailService
+
 router = APIRouter()
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 settings = get_settings()
 @router.get("/users/{user_id}", response_model=UserResponse, name="get_user", tags=["User Management Requires (Admin or Manager Roles)"])
